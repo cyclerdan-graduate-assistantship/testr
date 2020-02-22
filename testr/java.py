ba1e48d7a6f7687f36b68a9e404ea10c40ac2728
Daniel Ricker <daniel.ricker@scranton.edu>
2020-02-22 14:23:10 -0500

    Fixed java

--- testr/java.py
+++ testr/java.py
@@ -15,16 +15,16 @@ class Java(object):
         run_file_name = File.remove_extension_from_file_name(File.remove_path_from_file_name(run_file_path))
         print("running the java program")
         if input_file_path is None:
-            shell_command = ['exec java -classpath ' + run_file_directory_path + ' ' + run_file_name]
+            shell_command = ['exec /shared/jdk-13.0.2/bin/java -classpath ' + run_file_directory_path + ' ' + run_file_name]
             print(shell_command[0])
             run_result = Shell.execute_shell_command(shell_command=shell_command, use_shell=True)
         else:
             if get_input_from_command_line:
-                shell_command = 'exec java -classpath ' + run_file_directory_path + '/ ' + run_file_name + ' ' + File().get_text_from_file(input_file_path)
+                shell_command = 'exec /shared/jdk-13.0.2/bin/java -classpath ' + run_file_directory_path + '/ ' + run_file_name + ' ' + File().get_text_from_file(input_file_path)
                 print(shell_command[0])
                 run_result = Shell.execute_shell_command(shell_command, True)
             else:
-                shell_command = 'exec java -classpath ' + run_file_directory_path + '/ ' + run_file_name + ' < ' + input_file_path + ' > ' + run_file_directory_path + '/out.txt'
+                shell_command = 'exec /shared/jdk-13.0.2/bin/java -classpath ' + run_file_directory_path + '/ ' + run_file_name + ' < ' + input_file_path + ' > ' + run_file_directory_path + '/out.txt'
                 print(shell_command[0])
                 run_result = Shell.execute_shell_command(shell_command, True)
                 run_result.output = File().get_text_from_file(run_file_directory_path + 'out.txt')
