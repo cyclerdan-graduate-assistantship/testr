88ff701d2c2a95e84dcc99a9e5d3ef628f86adfb
Daniel Ricker <daniel.ricker@scranton.edu>
2020-02-24 14:21:08 -0500

    Fixed extraneous printing to console

--- testr/java.py
+++ testr/java.py
@@ -13,19 +13,19 @@ class Java(object):
     def run(cls, run_file_path, get_input_from_command_line=False, input_file_path=None):
         run_file_directory_path = File.get_path_from_file_name(run_file_path)
         run_file_name = File.remove_extension_from_file_name(File.remove_path_from_file_name(run_file_path))
-        print("running the java program")
+        # print("running the java program")
         if input_file_path is None:
             shell_command = ['exec /shared/jdk-13.0.2/bin/java -classpath ' + run_file_directory_path + ' ' + run_file_name]
-            print(shell_command[0])
+            # print(shell_command[0])
             run_result = Shell.execute_shell_command(shell_command=shell_command, use_shell=True)
         else:
             if get_input_from_command_line:
                 shell_command = 'exec /shared/jdk-13.0.2/bin/java -classpath ' + run_file_directory_path + '/ ' + run_file_name + ' ' + File().get_text_from_file(input_file_path)
-                print(shell_command[0])
+                # print(shell_command[0])
                 run_result = Shell.execute_shell_command(shell_command, True)
             else:
                 shell_command = 'exec /shared/jdk-13.0.2/bin/java -classpath ' + run_file_directory_path + '/ ' + run_file_name + ' < ' + input_file_path + ' > ' + run_file_directory_path + '/out.txt'
-                print(shell_command[0])
+                # print(shell_command[0])
                 run_result = Shell.execute_shell_command(shell_command, True)
                 run_result.output = File().get_text_from_file(run_file_directory_path + 'out.txt')
         return run_result
