57222735373b80278e95bacf9adf4eb51c0359ac
Minionice <minionice@gmail.com>
2020-09-18 10:37:46 -0400

    Appends documents and quizzes correctly assuming they are named correctly

--- testr/word_document.py
+++ testr/word_document.py
@@ -3,6 +3,7 @@ from shutil import copyfile
 from docx.shared import Pt
 from docx.shared import Inches
 from docx import *
+import docx
 from .file import File
 
 
@@ -27,7 +28,8 @@ class WordDocument(object):
                 index_of_prompt_string = len(interleave_string) - len(prompt_string)
                 input_from_keyboard = " "
                 interleave_error = True
-            result = result +"\n"+ interleave_string[:(index_of_prompt_string + len(prompt_string))] + input_from_keyboard
+            result = result + "\n" + interleave_string[
+                                     :(index_of_prompt_string + len(prompt_string))] + input_from_keyboard
             interleave_string = interleave_string[index_of_prompt_string + len(prompt_string):]
             interleave_string = interleave_string
             if interleave_error:
@@ -38,11 +40,15 @@ class WordDocument(object):
 
     @classmethod
     def assemble_feedback_document(cls, testr, directory_of_template):
-        print("Working directory: "+testr.working_directory)
+        print("Working directory: " + testr.working_directory)
         created_feedback_document_successfully = True;
         try:
             copyfile(directory_of_template, testr.working_directory + '/Feedback.docx')
             feedback_document = Document(testr.working_directory + '/Feedback.docx')
+
+            copyfile(directory_of_template, testr.working_directory + '/tmp.docx')
+            tmp_document = Document(testr.working_directory + '/tmp.docx')
+
             style = feedback_document.styles['Normal']
             font = style.font
             font.name = 'Courier New'
@@ -56,16 +62,24 @@ class WordDocument(object):
             for comment_file in testr.comment_files:
                 feedback_document.add_paragraph(File.get_text_from_file(comment_file), None)
             feedback_document.add_page_break()
-            feedback_document.add_paragraph(File.get_text_from_file(testr.path_to_source_file), None)
-            #
-            feedback_document.add_page_break()
-            for compile_shell_output in testr.list_of_compile_shell_outputs:
-                feedback_document.add_paragraph(compile_shell_output.output, None)
-            for run_shell_output in testr.list_of_run_shell_outputs:
-                feedback_document.add_paragraph(run_shell_output.output, None)
+
+            quiz_doc = docx.Document(testr.working_directory + '/Quiz02v1.docx')
+            # quiz_doc = docx.Document(testr.working_directory + '/Quiz02v2.docx')
+            feedback_document.element.body.append(quiz_doc.element.body)
             feedback_document.save(testr.working_directory + '/Feedback.docx')
-            if not testr.testr_configuration.test_input_from_cli and len(testr.testr_configuration.test_cases) > 0:
-                os.remove(testr.working_directory + '/out.txt')
+            tmp_document.add_page_break()
+
+            tmp_document.add_paragraph(File.get_text_from_file(testr.path_to_source_file), None)
+
+            tmp_document.add_page_break()
+            for compile_shell_output in testr.list_of_compile_shell_outputs:
+                tmp_document.add_paragraph(compile_shell_output.output, None)
+            for run_shell_output in testr.list_of_run_shell_outputs:
+                tmp_document.add_paragraph(run_shell_output.output, None)
+            # if not testr.testr_configuration.test_input_from_cli and len(testr.testr_configuration.test_cases) > 0:
+            #     os.remove(testr.working_directory + '/out.txt')
+            feedback_document.element.body.append(tmp_document.element.body)
+            feedback_document.save(testr.working_directory + '/Feedback.docx')
         except Exception as e:
             print(e)
             created_feedback_document_successfully = False
@@ -74,8 +88,7 @@ class WordDocument(object):
     def assemble_feedback_document_txt(testr, directory_of_template):
         created_feedback_document_successfully = True;
         try:
-            feedback_document = open(testr.working_directory + '/Feedback.txt',"w")
-
+            feedback_document = open(testr.working_directory + '/Feedback.txt', "w")
 
             for comment_file in testr.comment_files:
                 feedback_document.write(File.get_text_from_file(comment_file))
